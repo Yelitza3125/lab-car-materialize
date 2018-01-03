81331b83c85109e764a08e0a2bcb26462b44acfa
Yelitza3125 <deyel_99@hotmail.com>
2018-01-03 13:51:43 -0500

    Modal de inicio de sesión

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 // Initialize collapse button
-$(".button-collapse").sideNav();
+
 // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 //$('.collapsible').collapsible();
 
-/*
-$(document).ready(function modal(){
-    $('.modal-trigger').modal();
-}); */
+$(document).ready(function(){
+  $('.modal').modal();
 
+    $(".button-collapse").sideNav();
+  });
+         
